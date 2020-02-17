896c1737bb5aaaadb2826102c1879fe442ea6d23
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-02-17 21:32:50 +0300

    add terrible middle

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -9,7 +9,7 @@ void main()
 {
 	QString path;
 	QTextStream cin(stdin);
-	QImage image, imageReverse, imageMatrix;
+	QImage image, imageReverse, imageMiddle;
 
 	cout << "Enter the path to image: ";
 	cin >> path;
@@ -17,31 +17,35 @@ void main()
 	image.load(path);
 	cout << "Image is loaded..." << endl;
 	imageReverse = image;
-	imageMatrix = image;
+	imageMiddle = image;
 
 	// Reverse
 	for(int i = 0; i < image.width(); i++)
 		for (int j = 0; j < image.height(); j++)
 		{
 			QColor color = image.pixel(i, j);
-			color.setRed(255 - color.red());
-			color.setGreen(255 - color.green());
-			color.setBlue(255 - color.blue());
-			imageReverse.setPixelColor(i, j, color);
+			imageReverse.setPixelColor(i, j, QColor(255 - color.red(), 255 - color.green(), 255 - color.blue()));
 		}
 
-	imageReverse.save("Reverse.png", "PNG");
-	cout << "Reverse image is saved as 'Reverse.png'...";
+	imageReverse.save("reverse.png", "PNG");
+	cout << "Reverse image is saved as 'reverse.png'..." << endl;
 
-	//// Matrix
-	//for (int i = 0; i < image.width(); i++)
-	//	for (int j = 0; j < image.height(); j++)
-	//	{
-	//		QColor color = image.pixel(i, j);
-	//		for (int x = i - 1; x < i + 2; x++)
-	//			for (int y = j - 1; y < j + 2; y++)
-	//				color += image.pixel(x, y);
-	//		
-	//		imageReverse.setPixelColor(i, j, color);
-	//	}
+	// Middle
+	for (int i = 1; i < image.width() - 1; i++)
+		for (int j = 1; j < image.height() - 1; j++)
+		{
+			QColor color;
+			for (int x = i - 1; x < i + 2; x++)
+				for (int y = j - 1; y < j + 2; y++)
+				{
+					QColor colorPixel = image.pixel(x, y);
+					color.setRed(color.red() + colorPixel.red() / 9);
+					color.setGreen(color.green() + colorPixel.green() / 9);
+					color.setBlue(color.blue() + colorPixel.blue() / 9);
+				}
+			imageMiddle.setPixelColor(i, j, color);
+		}
+
+	imageMiddle.save("middle.png", "PNG");
+	cout << "Middle image is saved as 'middle.png'..." << endl;
 }

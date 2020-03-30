ab9f4d26d270ae3a0343474ccba5cb5551ec3bc9
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-03-30 18:57:21 +0300

    add fps

--- Lab2/TomogrammVisualizer/Form1.Designer.cs
+++ Lab2/TomogrammVisualizer/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.glControl1 = new OpenTK.GLControl();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,14 +63,25 @@
             this.trackBar1.TabIndex = 2;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(135, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 30);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.glControl1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Tomogram Visualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -83,6 +95,7 @@
         private System.Windows.Forms.Button button1;
         private OpenTK.GLControl glControl1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Lab2/TomogrammVisualizer/Form1.cs
+++ Lab2/TomogrammVisualizer/Form1.cs
@@ -16,6 +16,8 @@ namespace TomogrammVisualizer
         public View view;
         public bool loaded;
         public int currentLayer;
+        int FrameCount;
+        DateTime NextFPSUpdate;
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +26,8 @@ namespace TomogrammVisualizer
             loaded = false;
             currentLayer = 0;
             trackBar1.Hide();
+            FrameCount = 0;
+            NextFPSUpdate = DateTime.Now.AddSeconds(1);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -54,6 +58,32 @@ namespace TomogrammVisualizer
         {
             currentLayer = trackBar1.Value;
             glControl1.Invalidate();
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+            {
+                DisplayFPS();
+                glControl1.Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
+        }
+
+        private void DisplayFPS()
+        {
+            if (DateTime.Now > NextFPSUpdate)
+            {
+                label1.Text = String.Format("FPS {0}", FrameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                FrameCount = 0;
+            }
+
+            FrameCount++;
         }
     }
 }
